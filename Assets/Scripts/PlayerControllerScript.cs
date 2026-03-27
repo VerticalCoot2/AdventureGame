ddc5a49a9a7262e322d9c2d57f6bc44e0d4f681d
verticalcoot2 <verticalcoot2@gmail.com>
2026-03-27 16:48:50 +0100

    komity

--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -2,14 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControllerScript : BaseClassScript
+public class PlayerControllerScript : CreateEntity
 {
-    [SerializeField] GameObject Player; //Bacon
+    GameObject Player; //Bacon
     Rigidbody2D rb;
+    CircleCollider2D circCol;
+
+    [SerializeField] float jumpForce = 400;
+    [SerializeField] float horizontalMove;
+    [SerializeField] bool jump = false;
+
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+
+    [SerializeField] GameObject barbSword;
+    [SerializeField] GameObject tankShield;
+    [SerializeField] GameObject mageBook;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        circCol = GetComponent<CircleCollider2D>();
         Player = this.gameObject;
     }
     private void Start()
@@ -17,13 +29,34 @@ public class PlayerControllerScript : BaseClassScript
         BarbarianClass();
         //TankClass();
         //MageClass();
-        StartCoroutine(Launch());
     }
-    IEnumerator Launch()
+
+    void Update()
     {
-        yield return new WaitForSeconds(5);
-        rb.AddForce(new Vector2(50, 200) * 5);
+        horizontalMove = Input.GetAxis("Horizontal") * agility;
+
+        if (horizontalMove < 0f) transform.localEulerAngles = new Vector3(0, 180, 0);
+        if (horizontalMove > 0f) transform.localEulerAngles = new Vector3(0, 0, 0);
+
+        if(Input.GetKeyDown(jumpKey)) jump = true;
     }
+
+    void FixedUpdate()
+    {
+        Moving(horizontalMove, jump);  
+    }
+
+    void Moving(float movement, bool canJump)
+    {
+        rb.velocity = new Vector2(movement * 3 * agility * Time.fixedDeltaTime, rb.velocity.y);
+
+        if(canJump && circCol.IsTouchingLayers())
+        {
+            rb.AddForce(new Vector2(0, jumpForce));
+            jump = !canJump;
+        }
+    }
+
     void BarbarianClass()
     {
         className = "Barbarian";
@@ -33,6 +66,8 @@ public class PlayerControllerScript : BaseClassScript
         agility = 7;
         damage = strength / inteligence;
         canShoot = false;
+        var sword = Instantiate(barbSword, gameObject.transform);
+        sword.transform.position += new Vector3(0.6f, 0.3f, 0);
     }
 
     void TankClass()
